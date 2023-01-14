5d94e4307b341ada726f2ca711ef86ce2dfac3c5
darshak kakadiya <90410040+darshak3112@users.noreply.github.com>
2023-01-14 22:05:30 +0530

    blog page

--- pages/blog.js
+++ pages/blog.js
@@ -1,8 +1,78 @@
 import React from 'react'
+import styles from "@/styles/blog.module.css";
+import { Inter } from "@next/font/google";
+import Link from 'next/link';
+
+
+const inter = Inter({ subsets: ["latin"] });
 
 const blog = () => {
   return (
-    <div>this is blog home page</div>
+    <div className={`${styles.grid} ${styles.main}`}>
+      <div className={styles.card}>
+        <div className="blogitems">
+          <h2 className={inter.className}>
+            How to learn JavaScript in 2023?
+          </h2>
+          <p className={inter.className}>
+            JavaScript is the languague use to desing logic on the web.
+          </p>
+        </div>
+      </div>
+      <div className={styles.card}>
+        <div className="blogitems">
+          <h2 className={inter.className}>
+            <Link href={'/blogpost/hello'}>
+              How to learn JavaScript in 2023 11?
+              </Link>
+          </h2>
+          <p className={inter.className}>
+            JavaScript is the languague use to desing logic on the web.
+          </p>
+        </div>
+      </div>
+      <div className={styles.card}>
+        <div className="blogitems">
+          <h2 className={inter.className}>
+            How to learn JavaScript in 2023?
+          </h2>
+          <p className={inter.className}>
+            JavaScript is the languague use to desing logic on the web.
+          </p>
+        </div>
+      </div>
+      <div className={styles.card}>
+        <div className="blogitems">
+          <h2 className={inter.className}>
+            How to learn JavaScript in 2023?
+          </h2>
+          <p className={inter.className}>
+            JavaScript is the languague use to desing logic on the web.
+          </p>
+        </div>
+      </div>
+      <div className={styles.card}>
+        <div className="blogitems">
+          <h2 className={inter.className}>
+            How to learn JavaScript in 2023?
+          </h2>
+          <p className={inter.className}>
+            JavaScript is the languague use to desing logic on the web.
+          </p>
+        </div>
+      </div>
+      <div className={styles.card}>
+        <div className="blogitems">
+          <h2 className={inter.className}>
+            How to learn JavaScript in 2023?
+          </h2>
+          <p className={inter.className}>
+            JavaScript is the languague use to desing logic on the web.
+          </p>
+        </div>
+      </div>
+      
+    </div>
   )
 }
 
--- pages/blogpost/[slug].js
+++ pages/blogpost/[slug].js
@@ -1,12 +1,18 @@
 import { useRouter } from 'next/router'
 import React from 'react'
-
+import styles from "@/styles/Home.module.css";
+import blog from "@/styles/blog.module.css"
 
 const slug = () => {
   const router = useRouter();
   const { slug } = router.query;
   return (
-    <div>{slug}</div>
+    <div className={styles.container}>
+      <main className={blog.m1}>
+        <h1>Title of the page</h1><hr />
+        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, esse voluptas quia rem accusantium maiores, possimus ipsa vero repudiandae veritatis, voluptatum consequuntur facere.</div>
+      </main>
+    </div>
   )
 }
 
