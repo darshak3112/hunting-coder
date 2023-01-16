42ecd876f613f455195843ef8997b49fbd1732d6
darshak kakadiya <90410040+darshak3112@users.noreply.github.com>
2023-01-16 13:56:51 +0530

    blog api  done on clinet side

--- pages/blogpost/[slug].js
+++ pages/blogpost/[slug].js
@@ -1,16 +1,27 @@
 import { useRouter } from 'next/router'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from "@/styles/Home.module.css";
 import blog from "@/styles/blog.module.css"
 
 const slug = () => {
   const router = useRouter();
+  const [blogs, setBlogs] = useState([]);
   const { slug } = router.query;
+  useEffect(() => {
+    console.log("useeffect is running");
+    fetch(`http://localhost:3000/api/getblogs?slug=${slug}`).then((a) => {
+      return a.json();
+    }).then((parsed) => {
+      console.log(parsed)
+      setBlogs(parsed)
+    })
+  }, [])
+
   return (
     <div className={styles.container}>
       <main className={blog.m1}>
-        <h1>Title of the page</h1><hr />
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, esse voluptas quia rem accusantium maiores, possimus ipsa vero repudiandae veritatis, voluptatum consequuntur facere.</div>
+        <h1>{blogs.title}</h1><hr />
+        <div>{blogs.content}</div>
       </main>
     </div>
   )
